d7ee2f11567fb6dab1e2cc66cfe547c3814d8d49
vacheto <115094780+niko-nikolov@users.noreply.github.com>
2022-10-06 18:37:33 +0300

    Create 05. Godzilla vs. Kong.js

---
+++ ProgrammingBasics/05. Godzilla vs. Kong.js
@@ -0,0 +1,25 @@
+function GodzillaVsKong(input) {
+  const budget = Number(input[0]);
+  const decore = budget * 0.1;
+  const actors = Number(input[1]);
+  const suitperactor = Number(input[2]);
+
+  if (actors > 150) {
+    suitperactor = suitperactor - suitperactor * 0.1;
+  }
+  const difference = decore + suitperactor * actors;
+  if (difference > budget) {
+    console.log(
+      `Not enough money!
+      Wingard needs ${(difference - budget).toFixed(2)} leva more.`
+    );
+  } else {
+    console.log(
+      `Action!
+      Wingard starts filming with ${(budget - difference).toFixed(
+        2
+      )} leva left.`
+    );
+  }
+}
+GodzillaVsKong(["15437.62", "186", "57.99"]);
